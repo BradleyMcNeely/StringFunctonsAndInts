c1b3f9b1eb5063765683c5eec6dfc70650a768bc
Bradley McNeely <CMP_BrMcNeely@ects.org>
2023-09-26 09:33:04 -0400

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Runtime.Remoting.Channels
+
+Module Module1
+    'Bradley McNeely
+    '9/26/23
+    'Basic String Functions and Integers!
+    Sub Main()
+        Dim Input As String
+        Dim city, state As String
+        Dim commaLocation, stringLength As Integer
+        Console.Write("Please enter a city and state seperated by only a comma (no spaces) >> ")
+        Input = Console.ReadLine()
+        'our goal is to sperate this into two different string variables (city and state)
+        'we assume we have a string in the format city, state
+        'find the comma
+        commaLocation = Input.IndexOf(",") 'FIND THE COMMA
+        Console.WriteLine("the comma is located at position {0}", commaLocation)
+        'take the first part of the string up to the comma, that is the city
+        city = Input.Substring(0, commaLocation)
+        Console.WriteLine("the city is {0}", city)
+        stringLength = Input.Length 'FIND THE LENGTH
+        Console.WriteLine("the length of the string is {0}", stringLength)
+        'take the last part of the string after to comma, that is the state
+        state = Input.Substring(commaLocation + 1, stringLength - commaLocation - 1)
+        Console.WriteLine("the state is  {0}", state)
+        'state = Input.Substring(commaLocation )
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
---
+++ StringRePlace.vb
@@ -0,0 +1,18 @@
+﻿Module StringRePlace
+    'Bradley Mcneely
+    '9/26/23
+    'String Replace
+
+    Sub main()
+        Dim input As String
+        Dim newString As String
+        Dim originalString As String = "Mr. Klins is the teacher of this class"
+        Console.WriteLine(originalString)
+        Console.Write("What should we change Mr. Klins' name to? -> ")
+        input = Console.ReadLine
+        newString = originalString.Replace("Mr. Klins", input) 'STRING REPLACEMENT!!!!! 
+        Console.WriteLine("The new string is {0} characters long as is as follows: ", newString.Length)
+        Console.WriteLine(newString)
+        Console.ReadLine()
+    End Sub
+End Module
